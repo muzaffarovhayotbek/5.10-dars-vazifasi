5699b17c1aea622a718133e4af8248648e50ee19
muzaffarovhayotbek <hayotbek0511@gmail.com>
2024-11-08 11:09:32 +0500

    end js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector('.btn button');
+const firstInput = document.querySelector('.firts-input')
+const twoInput = document.querySelector('.two-input');
+
+function validate(firstInput, twoInput) {
+  if (firstInput.value !== '123456' || twoInput.value !== '123456') {
+    alert('Username yoki parol noto‘g‘ri. Ikkalasi ham "123456" bo`lishi kerak');
+    return false;
+  }
+  return true;
+}
+
+btn.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const isValid = validate(twoInput);
+  if (!isValid) {
+    return;
+  }
+
+  window.location.href = '/pages/home.html';
+
+});
